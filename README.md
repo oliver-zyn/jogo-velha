b06e1d6744e80e89c0edd28caca828b1f38cc7d7
oliver-zyn <olivermayer15@gmail.com>
2022-02-22 21:55:47 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Jogo da Velha
+
+- Jogo da velha clássico com dois jogadores (X e O)
---
+++ js/interface.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
+  });
+});
+
+function handleClick(event) {
+  let square = event.target;
+  let position = square.id;
+
+  if (handleMove(position)) {
+    setTimeout(() => {
+      if (playerTime != 0) {
+        alert("O Jogo Acabou - O Vencedor foi o jogador X");
+      } else if (playerTime != 1) {
+        alert("O Jogo Acabou - O Vencedor foi o jogador O");
+      }
+    }, 10);
+  }
+  updateSquare(position);
+}
+
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+
+  if (symbol != "") {
+    square.innerHTML = `<div class=${symbol}><div>`;
+  }
+}
+
+function updateResetSquares() {
+  let o = document.querySelectorAll(".o");
+  let x = document.querySelectorAll(".x");
+
+  o.forEach((o) => {
+    o.classList.remove("o");
+  });
+
+  x.forEach((x) => {
+    x.classList.remove("x");
+  });
+}
